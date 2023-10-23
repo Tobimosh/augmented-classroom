420b668093c93de935d3a5ce508503f2cfe6af38
Moshood Oluwatobi <oluwatobimoshood16@gmail.com>
2023-10-23 17:05:37 +0100

    fixed a routing issue

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import LectureSlides from "./components/LectureSlides";
 const App = () => (
   <div>
     <Routes key={location.pathname} location={location}>
+      <Route path="/" element={<SignUp />} />
       <Route path="/sign-up" element={<SignUp />} />
       <Route path="/log-in" element={<Form />} />
       <Route path="/services" element={<Services />} />
