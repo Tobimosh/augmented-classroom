9b6a04bea808c9c86e1c0ba4e67f0aff9b1d0138
Moshood Oluwatobi <oluwatobimoshood16@gmail.com>
2023-11-02 17:41:13 +0100

    fixed a bug

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -21,12 +21,26 @@ const SignUp = () => {
   const [isLoading, setIsLoading] = useState(false);
   // const [isSignUpSuccessful, setIsSignUpSuccessful] = useState(false);
 
+
+
   const {
     register,
     handleSubmit,
     formState: { errors },
     reset,
   } = useForm<FormData>({ resolver: zodResolver(schema), mode: "onBlur" });
+
+    const notify = () =>
+      toast.success("Sign up successful", {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
 
   const handleFormSubmit = (formData: FormData) => {
     setIsLoading(true);
@@ -38,7 +52,7 @@ const SignUp = () => {
           console.log("Registration successful");
           reset();
           notify();
-          window.location.href = "/services";
+          // window.location.href = "/log-in";
         } else {
           console.error("Registration failed");
         }
@@ -56,84 +70,75 @@ const SignUp = () => {
       });
   };
 
-  const notify = () =>
-    toast.success("Sign up successful", {
-      position: "top-right",
-      autoClose: 5000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-      theme: "light",
-    });
-
   return (
-    <div className="flex justify-center h-[100vh] items-center">
-      <div className="flex items-center gap-10 bg-red-100">
-        <form
-          onSubmit={handleSubmit(handleFormSubmit)}
-          className="flex flex-col p-7 md:w-1/2 h-1/2 rounded-xl"
-        >
-          <div className="mb-5">
-            <h2 className="text-3xl">Let's sign you up!</h2>
-          </div>
-          <div className="mb-5 flex flex-col">
-            <label htmlFor="matric_number" className="mb-5">
-              Matric Number
-            </label>
-            <input
-              type="text"
-              {...register("matric_number")}
-              className="py-3 px-4 border border-gray-400 outline-none rounded-lg"
-              placeholder="Enter your matric number..."
-            />
-            {errors.matric_number && (
-              <p className="text-red-500">{errors.matric_number.message}</p>
-            )}
-          </div>
-          <div>
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              {...register("password")}
-              className="py-3 px-4 border border-gray-400 mt-5 outline-none w-full rounded-lg"
-              placeholder="********"
-            />
-          </div>
-          {errors.password && (
-            <p className="text-red-500">{errors.password.message}</p>
-          )}
-          <div className="flex justify-between mt-7 mb-5">
-            <button
-              type="submit"
-              className="bg-blue-200 w-full rounded-lg px-4 py-3"
-            >
-              Sign Up
-            </button>
-          </div>
-          <div className="flex items-end text-sm">
-            <span className="mr-2">Have an account?</span>
-            <Link to="/log-in" className="text-red-500">
-              Log in
-            </Link>
-          </div>
-        </form>
-        <div>
-          <img src="/img.png" className="h-[600px]" alt="" />
-        </div>
-      </div>
-      {/* )} */}
-      {isLoading && (
-        <Backdrop
-          sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
-          open={isLoading}
-        >
-          <CircularProgress color="inherit" />
-        </Backdrop>
-      )}
+    <>
       <ToastContainer />
-    </div>
+
+      <div className="flex justify-center h-[100vh] items-center">
+        <div className="flex items-center gap-10 bg-red-100">
+          <form
+            onSubmit={handleSubmit(handleFormSubmit)}
+            className="flex flex-col p-7 md:w-1/2 h-1/2 rounded-xl"
+          >
+            <div className="mb-5">
+              <h2 className="text-3xl">Let's sign you up!</h2>
+            </div>
+            <div className="mb-5 flex flex-col">
+              <label htmlFor="matric_number" className="mb-5">
+                Matric Number
+              </label>
+              <input
+                type="text"
+                {...register("matric_number")}
+                className="py-3 px-4 border border-gray-400 outline-none rounded-lg"
+                placeholder="Enter your matric number..."
+              />
+              {errors.matric_number && (
+                <p className="text-red-500">{errors.matric_number.message}</p>
+              )}
+            </div>
+            <div>
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                {...register("password")}
+                className="py-3 px-4 border border-gray-400 mt-5 outline-none w-full rounded-lg"
+                placeholder="********"
+              />
+            </div>
+            {errors.password && (
+              <p className="text-red-500">{errors.password.message}</p>
+            )}
+            <div className="flex justify-between mt-7 mb-5">
+              <button
+                type="submit"
+                className="bg-blue-200 w-full rounded-lg px-4 py-3"
+              >
+                Sign Up
+              </button>
+            </div>
+            <div className="flex items-end text-sm">
+              <span className="mr-2">Have an account?</span>
+              <Link to="/log-in" className="text-red-500">
+                Log in
+              </Link>
+            </div>
+          </form>
+          <div>
+            <img src="/img.png" className="h-[600px]" alt="" />
+          </div>
+        </div>
+        {/* )} */}
+        {isLoading && (
+          <Backdrop
+            sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+            open={isLoading}
+          >
+            <CircularProgress color="inherit" />
+          </Backdrop>
+        )}
+      </div>
+    </>
   );
 };
 
