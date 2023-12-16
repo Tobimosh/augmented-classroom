fc2dcd0e30e4b314437dc3d97a56bd889abd7700
Moshood Oluwatobi <oluwatobimoshood16@gmail.com>
2023-12-16 17:34:48 +0100

    updated the register hook to include authorization header with bearer token

--- src/hooks/useRegister.ts
+++ src/hooks/useRegister.ts
@@ -1,27 +1,3 @@
-// import { useMutation } from "react-query";
-// import { FormData } from "../pages/SignUp";
-// import APIClient from "../services/api-client";
-// import { useNavigate } from "react-router-dom";
-
-
-// const apiClient = new APIClient<FormData>('/create-student')
-
-
-// const submitForm = (data: FormData) =>
-//      apiClient.register(data)
-
-// export const useRegister = () =>{
-// const navigate = useNavigate();
-//  return	useMutation({
-// 		mutationFn: submitForm,
-// 		onError: (error) => alert(error),
-// 		onSuccess: (data) => {
-// 			alert(data)
-// 			navigate('/log-in')
-// 		}
-// 	});
-// }
-
 
 import { useMutation } from "react-query";
 import { useNavigate } from "react-router-dom";
@@ -69,3 +45,4 @@ export const useRegister = () => {
     }
   });
 };
+ 
