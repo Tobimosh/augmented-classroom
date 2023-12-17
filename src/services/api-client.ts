0e447e934ef27d33595e13876791360c20305b65
Moshood Oluwatobi <oluwatobimoshood16@gmail.com>
2023-12-17 01:05:17 +0100

    fixed a bug

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -4,18 +4,18 @@ import axios, { AxiosInstance } from "axios";
 import { ToastOptions, toast } from "react-toastify";
 
 const axiosInstance: AxiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 const toastStyle: ToastOptions = {
-  position: 'top-right',
+  position: "top-right",
   autoClose: 5000,
   hideProgressBar: false,
   closeOnClick: true,
   pauseOnHover: true,
   draggable: true,
   progress: undefined,
-  theme: 'light',
+  theme: "light",
 };
 
 class APIClient<T> {
@@ -24,6 +24,8 @@ class APIClient<T> {
 
   constructor(endpoint: string) {
     this.endpoint = endpoint;
+    // Initial token check on instantiation
+    this.checkAndRefreshToken();
   }
 
   setBearerToken(token: string) {
@@ -31,72 +33,50 @@ class APIClient<T> {
   }
 
   register = (data: T) => {
-    return axiosInstance.post<T>(this.endpoint, data, {
-      headers: {
-        Authorization: this.authToken ? `Bearer ${this.authToken}` : '',
-      },
-    })
-    .then(res => {
+    return axiosInstance
+      .post<T>(this.endpoint, data, {
+        headers: {
+          Authorization: this.authToken ? `Bearer ${this.authToken}` : "",
+        },
+      })
+      .then((res) => {
         if (res.status === 200) {
-            toast.success("SUCCESS!!", toastStyle);
+          toast.success("SUCCESS!!", toastStyle);
         }
         return res.data;
       })
-      .catch(error => {
-        if (error.response) {
-          if (error.response.status === 404) {
-            toast.error("I'm afraid you put in the wrong details", toastStyle);
-          } else if (error.response.status === 400) {
-            toast.error("Student with that details already exists", toastStyle);
-          }
-        } else if (error.request) {
-          toast.error("No response from the server", toastStyle);
-        } else {
-          toast.error("Error in request setup", toastStyle);
-        }
-        console.error('Error:', error);
+      .catch((error) => {
+        // Handle errors and display toasts
+        this.handleRequestError(error);
         throw error;
       });
-  }
+  };
 
-
-  // login = (data: T) => {
-  //   return axiosInstance.post(this.endpoint, data)
-  //     .then(res => {
-  //       if (res.status === 200) {
-  //         const { access_token, token_type, refresh_token } = res.data;
-  //         this.setBearerToken(access_token);
-  //         return { access_token, token_type, refresh_token };
-  //       } else {
-  //         return Promise.reject("Login failed");
-  //       }
-  //     })
-  //     .catch(error => {
-  //       toast.error('Error:', error);
-  //       throw error;
-  //     });
-  // };
-
-login = (data: T) => {
-    return axiosInstance.post(this.endpoint, data)
-      .then(res => {
+  login = (data: T) => {
+    return axiosInstance
+      .post(this.endpoint, data)
+      .then((res) => {
         if (res.status === 200) {
-          const { access_token, token_type, refresh_token, expires_in } = res.data;
+          const { access_token, token_type, refresh_token, expires_in } =
+            res.data;
           this.setBearerToken(access_token);
 
           // Store tokens in local storage
-          localStorage.setItem('access_token', access_token);
-          localStorage.setItem('refresh_token', refresh_token);
+          localStorage.setItem("access_token", access_token);
+          localStorage.setItem("refresh_token", refresh_token);
 
           // Calculate token expiration time in milliseconds
-          const expirationTime = expires_in * 1000;
+          const expirationTime = 2 * 60 * 1000;
 
-          // Check if the token is about to expire (e.g., within next 4 minutes)
-          const currentTime = Math.floor(Date.now() / 1000);
-          if (expirationTime - currentTime < 240) {
+          const currentTime = performance.now();
+          if (expirationTime - currentTime < 90 * 1000) {
             // Token is about to expire, initiate token refresh
-            return this.refreshToken().then(newAccessToken => {
-              return { access_token: newAccessToken, token_type, refresh_token };
+            return this.refreshToken().then((newAccessToken) => {
+              return {
+                access_token: newAccessToken,
+                token_type,
+                refresh_token,
+              };
             });
           } else {
             return { access_token, token_type, refresh_token };
@@ -105,35 +85,69 @@ login = (data: T) => {
           return Promise.reject("Login failed");
         }
       })
-      .catch(error => {
-        toast.error('Error:', error);
+      .catch((error) => {
+        // Handle errors and display toasts
+        this.handleRequestError(error);
         throw error;
       });
   };
 
-
-    refreshToken = async () => {
-    const refreshToken = localStorage.getItem('refresh_token');
-    console.log(refreshToken)
-
+  refreshToken = async () => {
+    const refreshToken = localStorage.getItem("refresh_token");
     if (!refreshToken) {
-      throw new Error('Refresh token not available');
+      throw new Error("Refresh token not available");
     }
 
     try {
-      const response = await axiosInstance.post('/refresh', {
+      const response = await axiosInstance.post("/refresh", {
         refresh_token: refreshToken,
       });
 
       const newAccessToken = response.data.access_token;
       this.setBearerToken(newAccessToken);
 
+      // Schedule the next token check after 1 minute and 30 seconds
+      setTimeout(this.checkAndRefreshToken.bind(this), 90 * 1000);
+
       return newAccessToken;
     } catch (error) {
-      console.error('Error refreshing token:', error);
+      // Handle errors and display toasts
+      this.handleRequestError(error);
       throw error;
     }
-  }
+  };
+
+  private checkAndRefreshToken = () => {
+    const expirationTime = 2 * 60 * 1000;
+    const currentTime = performance.now();
+    if (expirationTime - currentTime < 90 * 1000) {
+      // Token is about to expire, initiate token refresh
+      this.refreshToken().catch((error) => {
+        console.error("Error refreshing token:", error);
+      });
+    } else {
+      // Schedule the next token check after 1 minute and 30 seconds
+      setTimeout(this.checkAndRefreshToken.bind(this), 90 * 1000);
+    }
+  };
+
+  private handleRequestError = (error: any) => {
+    if (error.response) {
+      if (error.response.status === 404) {
+        toast.error("I'm afraid you put in the wrong details", toastStyle);
+      } else if (error.response.status === 400) {
+        toast.error(
+          "Student with that details already exists",
+          toastStyle
+        );
+      }
+    } else if (error.request) {
+      toast.error("No response from the server", toastStyle);
+    } else {
+      toast.error("Error in request setup", toastStyle);
+    }
+    console.error("Error:", error);
+  };
 }
 
 export default APIClient;
