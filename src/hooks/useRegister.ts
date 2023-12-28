82603581c908d22f6d0fb63543b83eae1679917d
Moshood Oluwatobi <oluwatobimoshood16@gmail.com>
2023-12-28 02:51:00 +0100

    testing

--- src/hooks/useRegister.ts
+++ src/hooks/useRegister.ts
@@ -1,28 +1,25 @@
-
 import { useMutation } from "react-query";
 import { useNavigate } from "react-router-dom";
 import { ToastOptions, toast } from "react-toastify";
 import { FormData } from "../pages/SignUp";
 import APIClient from "../services/api-client";
+import useAttendance from "./useAttendance";
 
-
-const apiClient = new APIClient<FormData>('/create-student');
-
+const apiClient = new APIClient<FormData>("/create-student");
 
 const toastStyle: ToastOptions = {
-  position: 'top-right',
+  position: "top-right",
   autoClose: 5000,
   hideProgressBar: false,
   closeOnClick: true,
   pauseOnHover: true,
   draggable: true,
   progress: undefined,
-  theme: 'light',
+  theme: "light",
 };
 
-
 const submitForm = (data: FormData) => {
-  const authToken = import.meta.env.VITE_APP_BEARER_TOKEN; 
+  const authToken = import.meta.env.VITE_APP_BEARER_TOKEN;
 
   if (!authToken) {
     alert("Authentication token is missing");
@@ -34,21 +31,21 @@ const submitForm = (data: FormData) => {
   return apiClient.register(data);
 };
 
-
 export const useRegister = () => {
   const navigate = useNavigate();
-
+  const attendanceMutation = useAttendance();
 
   return useMutation({
     mutationFn: submitForm,
     onError: (error) => console.log(error),
-    onSuccess: () => {
-	  toast.success('Registration Successful, please log in', toastStyle);
-
+    onSuccess: async (data) => {
+      toast.success("Registration Successful, please log in", toastStyle);
+      await attendanceMutation.mutate({
+        username: data.matric_number,
+      });
       setTimeout(() => {
-        navigate('/log-in');
+        navigate("/log-in");
       }, 3000);
-    }
+    },
   });
 };
- 
