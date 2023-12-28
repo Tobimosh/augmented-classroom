485e50c099b5816581f849edf725a415cc934c65
Moshood Oluwatobi <oluwatobimoshood16@gmail.com>
2023-12-28 02:13:40 +0100

    testing

--- src/hooks/useAttendance.ts
+++ src/hooks/useAttendance.ts
@@ -2,12 +2,14 @@ import { useMutation } from "react-query";
 import APIClient from "../services/api-client";
 import { AttendanceData } from "../pages/Attendance";
 
-const apiClient = new APIClient<AttendanceData>(
-  "/generate-registration-options"
-);
+
 
 const submitForm = (data: AttendanceData) => {
   // const authToken = localStorage.getItem("access_token");
+
+  const apiClient = new APIClient<AttendanceData>(
+  `/generate-registration-options?matric_number=${data.username}`
+);
     const authToken = import.meta.env.VITE_APP_BEARER_TOKEN; 
 
 
